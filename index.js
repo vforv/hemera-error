e961ea0e41ecedb5d3cd11b8c615a9e4187d5ca2
Vladimir Djukic <vladimir@amerbank.com>
2017-09-15 14:39:50 -0500

    error update

--- index.js
+++ index.js
@@ -26,44 +26,93 @@ L.describe('Math', function () {
                 topic: 'user',
                 cmd: 'login'
             }, function (msg, cb) {
+                let __this = this;
+                let hemera = __this;
+
+                let userPeyload = msg.user;
+
+
+                let user = {
+                    firstName: userPeyload.firstName,
+                    lastName: userPeyload.lastName,
+                    email: userPeyload.email,
+                    password: 'hash',
+                    phone: userPeyload.phone,
+                    role: userPeyload.role,
+                    address: userPeyload.address,
+                    city: userPeyload.city,
+                    state: userPeyload.state,
+                    country: userPeyload.country,
+                    zip: userPeyload.zip,
+                    bankId: userPeyload.bankId
+                }
+
                 hemera.act({
-                    topic: 'user-credentials',
-                    cmd: 'verify',
-                    email: msg.email,
-                    password: msg.password
-                }, (err, user) => {
+                    topic: 'mongo-store',
+                    cmd: 'find',
+                    collection: 'users',
+                    query: { email: userPeyload.email }
+                }, (err, resp) => {
                     if (err) {
-                        done(err)
+                        done(err);
                     }
-                    console.log("HELLO")
-                    if (!user) {
+
+                    if (resp.result.length === 0) {
+
+                        hemera.act({
+                            topic: 'mongo-store',
+                            cmd: 'create',
+                            collection: 'users',
+                            data: user
+                        }, (err, resp) => {
+                            if (err) {
+                                done(null, err);
+                            }
+
+                            //TODO: call mail service here
+                            done(null, 'We sent you activation link to the mail.');
+                        })
+
+                    } else {
                         const UnauthorizedError = hemera.createError("Unauthorized");
-                        const mess = new UnauthorizedError("User credentials not valid.");
+                        const mess = new UnauthorizedError("User already exists.");
                         done(mess);
                     }
-
-                    this.act({
-                        topic: 'jwt',
-                        cmd: 'generate',
-                        userId: user._id
-                    }, (err, resp) => {
-                        if (err) {
-                            done(err)
-                        }
-                        
-                        console.log("HERE NOT WORKING")
-                        done(null, { token: resp })
-                    });
-
-                });
+                })
             });
+
+            const userData = {
+                firstName: 'Vladimir',
+                lastName: 'Djukic',
+                email: 'vladimir@some.com',
+                password: 'hash',
+                phone: '123123123',
+                role: ['admin'],
+                address: 'Cuk br.3',
+                city: 'Berane',
+                state: 'IL',
+                country: 'MNT',
+                zip: '123',
+                bankId: '123'
+            }
             // stub act calls
-            actStub.stub({ topic: 'jwt', cmd: 'generate', userId: '123' }, null, 50)
-            actStub.stub({ topic: 'user-credentials', cmd: 'verify', email: 'vladimir@some.com', password: 'test123' }, null, 300)
+            actStub.stub({
+                topic: 'mongo-store',
+                cmd: 'create',
+                collection: 'users',
+                data: userData
+            }, null, "Mail sent")
+
+            actStub.stub({
+                topic: 'mongo-store',
+                cmd: 'find',
+                collection: 'users',
+                query: { email: 'vladimir@some.com' }
+            }, null, { result: [] })
 
             // Important run it when "add" was already added
             // Should execute the server method with the pattern topic:math,cmd:add,a:100,b:200"
-            AddStub.run(hemera, { topic: 'user', cmd: 'login' }, { email: 'vladimir@some.com', password: 'test123' }, function (err, result) {
+            AddStub.run(hemera, { topic: 'user', cmd: 'login' }, { user: userData }, function (err, result) {
 
                 done()
             })
